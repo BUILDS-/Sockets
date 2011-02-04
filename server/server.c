18a8554d4e66a392fe4b08e8a9286374f00da4d9
Monica Gribouski <mgribs@csa2.bu.edu>
2011-02-03 22:04:45 -0500

    TCP Echo client and server now running in folder unpve13 2/tcpcliserv. Now must edit to accept commands.

--- server/server.c
+++ server/server.c
@@ -4,8 +4,13 @@
 int
 main(int argc, char **argv)
 {
-	printf("server main: HELLO\n");
+
+
+
+/*	printf("server main: HELLO\n");
 	dummy_hello();
 	printf("server main: GOODBYE\n");
 	return 0;
+*/
+
 } 
