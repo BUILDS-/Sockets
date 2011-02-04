2d6bb3e3df1aeddedae5bbe491ea877bc9819711
Liam Wang <lzw@csa2.bu.edu>
2011-02-04 03:57:35 -0500

    fixed the one bug (location of something to do with value connected too tired to know...) also commented it. Mostly done, but the capitalized comments are the bits that are still bothering me.

--- unpv13e 2/tcpcliserv/tcpcli01.c
+++ unpv13e 2/tcpcliserv/tcpcli01.c
@@ -6,122 +6,151 @@
 void 
 sendtext(char* fp, int sockfd)
 {
-printf("%s", fp);
-       // printf("%s\n", input);
-      //  printf("%s\n", input);
-char sendline[MAXLINE], recvline[MAXLINE];
-int s =0;
-while(fp[s-1]!='\0')
-{
-	Writen(sockfd, fp[s],1);
-	if(Readline(sockfd, recvline, MAXLINE) == 0)
+  printf("%s", fp);
+  
+  char sendline[MAXLINE], recvline[MAXLINE];
+  int s =0;
+  while(fp[s-1]!='\0')
+    {
+      Writen(sockfd, fp[s],1);
+      if(Readline(sockfd, recvline, MAXLINE) == 0)
 	{
-	err_quit("send: server terminated prematurely");
+	  err_quit("send: server terminated prematurely");
 	}
-	Fputs(recvline, stdout);
-	s++;
+      Fputs(recvline, stdout);
+      s++;
+    }
 }
-	}
 int
 main(int argc, char **argv)
 {
-	int c = 0;
-	int c2 = 0;
-	char test[200];
-	int connected = 0;
-	int					sockfd;
-	struct sockaddr_in	servaddr;
-	//char input[50] = {0};
-        //char input2[160] = {0};
-	char connect[] = "connect";
-	char send[] = "send";
-
-	printf("hivemind$");
-while(1){
-	char input[50]={0};
-	char input2[200]={0};
-	scanf("%s", input);
-if(connected==1)
-	scanf("%[^\n]s", input2);
-else
-	scanf("%s", input2);
-	
-        printf("%s\n", input);
-	 printf("%s\n", input2);
-
-	int menu = -1;
-        int i;
-	
-	for (i = 0; connect[i] != '\0'; i++) {
-	  char x = connect[i];
-	  if (x != input[i]) {
-	    menu = -1;
-	    break;
-	  }
-	  menu = 0;
+  //Checks to see if the socket has been connected yet or not.
+  int connected = 0;
+  int sockfd;
+  struct sockaddr_in	servaddr;
+  
+  //Global strings to compare inputs against.
+  char connect[] = "connect";
+  char send[] = "send";
+  
+  //Prompt user with our username.
+  printf("hivemind$");
+  
+  //While true loop
+  while(1){
+    
+    //Sets input and input2 to take in scanfs and sets them to the null set.
+    char input[50]={0};
+    char input2[200]={0};
+    
+    //Scans word one to input
+    scanf("%s", input);
+    
+    //If already connected. (for formatting purposes.)
+    //HEY GUYS! WHY CAN'T WE JUST USE %[^\n]s IN BOTH CASES?
+    //ALSO, I'M NOT REALLY IN THE STATE TO KNOW HOW TO ELIMINATE THE
+    //DAMN SPACE AT THE BEGINNING OF EVERY DAMN ECHO. KILL IT.
+    if(connected==1)
+      scanf("%[^\n]s", input2);
+    else
+      //otherwise, just run this.
+      scanf("%s", input2);
+    
+    
+    //Initializes the integer that stores whether the user wants to connect
+    //or send a string. Not hard coded.
+    int menu = -1;
+    
+    int i;
+    
+    //if the first word is "connect" menu is set to 0. Else set to -1.
+    for (i = 0; connect[i] != '\0'; i++) {
+      char x = connect[i];
+      if (x != input[i]) {
+	menu = -1;
+	break;
+      }
+      menu = 0;
+      connected = 1;
+    }
+    
+    //if the word isn't "connect" check to see if it's "send".
+    //if it is send, set menu to 1. Else set to -1.
+    if(menu != 0) {
+      for (i = 0; send[i] != '\0'; i++) {
+	char x = send[i];
+	if (x != input[i]) {
+	  menu = -1;
+	  break;
 	}
+	menu = 1;
+	
+      }
+    }
+    
+    
+    
+    if (menu == 0) {
+      //printf("debug: was connect.\n");
+      
+      //printf("%s\n", input);
+      
+      //Very not rooboost. (Levin-ism)
+      
+      //where the entered ip address will be stored.
+      char ip[20] = "\0";
+      //where the entered port will be stored.
+      char port[10] = "\0";
+      
+      //If the user wanted to connect. Read the ip address and
+      //the port number in.
+      if (menu == 0) {
+	for(i = 0; input2[i] != ':' && i < 35; i++) {
+	  //printf("debug: %d %c\n", i, input2[i]);
+	  ip[i] = input2[i];
+	}
+	int j;
+	i++;
+	for(j = 0; input2[i]; i++, j++) {
+	  port[j] = input2[i];
+	}
+      }
+      
+      //Translates the string of the port into an int
+      int i_port = atoi(port);
+      
+      //printf("debug: ip = %s \n", ip);
+      
 
-	if(menu != 0) {
-	  for (i = 0; send[i] != '\0'; i++) {
-	    char x = send[i];
-	    if (x != input[i]) {
-	      menu = -1;
-	      break;
-	    }
-		 menu = 1;
-		connected=1;	
+      sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+      
+      bzero(&servaddr, sizeof(servaddr));
+      servaddr.sin_family = AF_INET;
+      servaddr.sin_port = htons(i_port);
+      Inet_pton(AF_INET, ip, &servaddr.sin_addr);
+	  
+      Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+      
+
+      //Else, if the user wanted to send a string.
+    } else if (menu == 1) {
+      
+ 
+      //printf("debug: was send.\n");
+
+      //printf("debug: input2 = %s \n", input2);
+      sendtext(input2,sockfd);
+
+      printf("\n");
+      //If the user printed neither "connect" nor "send"
+    } else if (menu == -1) {
+      printf("failed comparison\n");
+    } else {
+      //If it gets here, something is very wrong.
+      printf("wtf: %d\n", menu);
+    }
+    //	sendtext(input2, sockfd);		/* do it all */
+    
+    //	exit(0);
   }
-	}
-
-	if (menu == 0) {
-	  printf("was connect.\n");
-       // scanf("%s", input2);
-	printf("%s\n", input);
-	
-	//Very not rooboost. (Levin-ism)
-	char ip[20] = "\0";
-	char port[10] = "\0";
-	if (menu == 0) {
-	  for(i = 0; input2[i] != ':' && i < 35; i++) {
-	    printf("%d %c\n", i, input2[i]);
-	    ip[i] = input2[i];
-	  }
-	  int j;
-	  i++;
-	  for(j = 0; input2[i]; i++, j++) {
-	    port[j] = input2[i];
-	  }
-	}
-	int i_port = atoi(port);
-
-	printf("ip = %s \n", ip);
-
-	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
-
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(i_port);
-	Inet_pton(AF_INET, ip, &servaddr.sin_addr);
-
-	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
-
-	//scanf("%s", input2);
-
-	} else if (menu == 1) {
-	
-       // scanf("%s", input2);
-  //printf("was send.\n");
-//	printf("i1%s\n", input);
-//	printf("i2%s\n", input2);
-//	printf("input 2 is equal to \n %s", input2);
-	 sendtext(input2,sockfd);
-//	printf("TEST");
-	} else if (menu == -1) {
-	  printf("failed comparison\n");
-	} else {
-	  printf("wtf %d\n", menu);
-	}
-//	sendtext(input2, sockfd);		/* do it all */
-
-//	exit(0);
-}}
+}
